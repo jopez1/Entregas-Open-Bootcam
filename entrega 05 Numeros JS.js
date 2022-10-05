cd2c29db5d775282fd1e9b8e2d66a35d65a76fd8
jopez1 <jopez1@hotmail.com>
2022-10-04 20:34:31 -0500

    entrega 05 Numeros JS

---
+++ entrega 05 Numeros JS.js
@@ -0,0 +1,7 @@
+let alturaCentimetros = 170;
+let alturaMetros = 1.70;
+let peso = 76.5;
+let alturaRedondeado = Math.ceil(alturaMetros);
+let pesoRedoendeado = Math.floor(peso) 
+let igualMaxvalorJs = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(igualMaxvalorJs)
