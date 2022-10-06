09e1f9ac967f367b42219a3e956d47b8b307fe5c
jopez1 <jopez1@hotmail.com>
2022-10-06 18:51:44 -0500

    Entrega 08 funciones

---
+++ Entrega 08 funciones.js
@@ -0,0 +1,17 @@
+// Una función sin parámetros que devuelva siempre "true"
+function devuelveTrue() {
+    return true
+}
+
+// Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+async function damePromesa() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+// Una función generadora de índices pares automáticos
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
